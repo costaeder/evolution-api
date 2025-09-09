3def505e9fe8f717a23cb03dc254e09f180088a2
Éder Costa <eder.almeida.costa@gmail.com>
2025-09-09 08:43:13 -0300

    Ajustes pra o podman

--- BuildImage.ps1
+++ BuildImage.ps1
@@ -1,4 +1,4 @@
-(Get-ECRLoginCommand).Password | docker login --username AWS --password-stdin 130811782740.dkr.ecr.us-east-2.amazonaws.com
+(Get-ECRLoginCommand).Password | podman login --username AWS --password-stdin 130811782740.dkr.ecr.us-east-2.amazonaws.com
 #
 
 $ErrorActionPreference = "Stop"
@@ -6,6 +6,6 @@ $ErrorActionPreference = "Stop"
  
  
 
-docker build -t evolution -f .\Dockerfile .
-docker tag evolution:latest 130811782740.dkr.ecr.us-east-2.amazonaws.com/evolution
-docker push 130811782740.dkr.ecr.us-east-2.amazonaws.com/evolution
+podman build -t evolution -f .\Dockerfile .
+podman tag evolution:latest 130811782740.dkr.ecr.us-east-2.amazonaws.com/evolution
+podman push 130811782740.dkr.ecr.us-east-2.amazonaws.com/evolution
